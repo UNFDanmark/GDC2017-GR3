55ad4a05f264914c6f22311c95ab4556bdfa0c0b
Gruppe 3 Programmører <silaswaeriksen@gmail.com>
2017-07-23 21:57:42 +0200

    Blob ka´ hop

---
+++ Unity/Super Smash Adventure/Assets/Scripts/PlayerScripts.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+	public float jumpHeight = 10;
+	public Rigidbody myRigidbody;
+    public float jumpsensitivity = 0.7f;
+
+	void awake() {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+
+    // Use this for initialization
+    void Start(){
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+            move(moveSpeed);
+        if (Input.GetKey(KeyCode.A))
+            move(-moveSpeed);
+        if (Input.GetKeyDown(KeyCode.W))
+            jump(jumpHeight);
+
+    }
+
+	void move (float speed) {
+        myRigidbody.velocity = transform.right * speed;
+    }
+
+    void jump(float height)
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, jumpsensitivity, 1 << 8))
+        {
+            myRigidbody.velocity = transform.up * height;
+        }
+    }
+	/*void FixedUpdate () {
+		
+	}*/
+
+}
