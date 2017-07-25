85c162b856256ae3afa69ba91fb7a2e2c9a6e0b4
Gruppe 3 Programmører <silaswaeriksen@gmail.com>
2017-07-26 00:41:34 +0200

    Toy fungerer

---
+++ Unity/Super Smash Adventure/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformScript : MonoBehaviour {
+
+    public float fallSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new  Vector3 (transform.position.x, transform.position.y-fallSpeed * Time.deltaTime, 0);
+	}
+}
