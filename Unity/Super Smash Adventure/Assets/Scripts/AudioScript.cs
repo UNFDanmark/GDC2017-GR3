efa57786b5ed51f21efce86aacfa75a23902f083
lyd <lyd@gr3.dk>
2017-07-27 14:33:15 +0200

    Adding sound effects

---
+++ Unity/Super Smash Adventure/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+
+public class AudioScript : MonoBehaviour
+{
+
+
+    //Lyd Klip Variable, kan indeholde et lydklip i sætter 
+    public AudioClip Hit;
+    public AudioClip Jump;
+    public AudioClip Falling;
+    //Definere Audio Source vi kalder den Audio(Husk at sætte den i inspector)
+    public AudioSource Audio;
+    public AudioSource Audio2;
+    //Hop funktionen husk den skal vaere public
+    public void HitSound()
+    {
+        Audio.clip = Hit;
+        Audio.Play();
+    }
+    public void JumpSound()
+    {
+        Audio2.clip = Jump;
+        Audio2.Play();
+    }
+    public void FallingSound()
+    {
+        Audio2.clip = Falling;
+        Audio2.Play();
+    }
+}
