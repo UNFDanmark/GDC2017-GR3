689e0121420492d54898553afca0bb130ad88522
Gruppe 3 Programmører <silaswaeriksen@gmail.com>
2017-07-27 09:53:47 +0200

    Working toy

--- Unity/Super Smash Adventure/Assets/Scripts/PlayerScripts.cs
+++ Unity/Super Smash Adventure/Assets/Scripts/PlayerScripts.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using UnityEngine;
+using UnityEngine.SceneManagement;
 
+//public class TurnAround :  MonoBehaviour { }
 public class PlayerScripts : MonoBehaviour {
 
     public GameObject otherPlayer;
@@ -10,15 +11,24 @@ public class PlayerScripts : MonoBehaviour {
 	public Rigidbody myRigidbody;
     public float rayLength = 1f;
     public float levelWidth = 15;
+    public float pushbackDistance = 5f;
+    public float pushForce = 2;
+    public float timeSinceBeingPush;
+    public float timeSincePushed;
+    public float pushLock = 0.2f;
+    public float pushTime = 0.2f;
+   
 
     public bool isPlayerTwo = false;
+    public bool pushed = false;
 
     public string horizontalAxis;
     public string verticalAxis;
 
     void awake() {
 		myRigidbody = GetComponent<Rigidbody> ();
-	}
+        timeSincePushed = -pushTime;
+    }
              
     // Use this for initialization
     void Start(){
@@ -26,8 +36,14 @@ public class PlayerScripts : MonoBehaviour {
     }
 
     // Upd8 15 c8113d 0nc3 p3r fr8me3
-	void Update () {
-       // motion1();
+    void Update() {
+        // motion1();
+        if (timeSinceBeingPush + pushLock < Time.time)
+        {
+            pushed = false;
+        }
+
+
 
         //Wall-teleport
         if (transform.position.x > levelWidth)
@@ -35,11 +51,33 @@ public class PlayerScripts : MonoBehaviour {
         else if (transform.position.x < -levelWidth)
             transform.position = new Vector3(levelWidth, myRigidbody.position.y, 0);
 
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerTwo == false)
+        {
+            Pushback();
+        }
+
+       if (Input.GetKeyDown(KeyCode.RightShift) && isPlayerTwo == true)
+        {
+            Pushback();
+        }
+    }
+    void Pushback()
+    {
+        if (Vector3.Distance(transform.position, otherPlayer.transform.position) < pushbackDistance && Time.time>timeSincePushed+pushTime)
+        {
+            timeSincePushed = Time.time;
+            print("Push ");
+            otherPlayer.GetComponent<PlayerScripts>().BeingPushed();
+            otherPlayer.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(otherPlayer.transform.position - gameObject.transform.position)*pushForce,ForceMode.Impulse);
+        }
     }
     void FixedUpdate()
     {
-        Move(moveSpeed * Input.GetAxis(horizontalAxis));
-        if(Input.GetAxis(verticalAxis) == 1) Jump();
+        if (!pushed)
+        {
+            Move(moveSpeed * Input.GetAxis(horizontalAxis));
+            if (Input.GetAxis(verticalAxis) == 1) Jump();
+        }
     }
 
     public void Move(float speed)
@@ -49,13 +87,20 @@ public class PlayerScripts : MonoBehaviour {
 
     public void Jump()
     {
-        print("JUMPY");
+        print("JUMPY (This still relevant?)");
         if (Physics.Raycast(transform.position - new Vector3(0.5f, 0, 0), Vector3.down, rayLength, 1 << 8) ||
          Physics.Raycast(transform.position + new Vector3(0.5f, 0, 0), Vector3.down, rayLength, 1 << 8))
         {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpHeight);
         }
-
+        //Access to DeathZone
+    }
+    void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "DeathZone")
+        {
+            print("You Lose");
+            Death();
+        }
     }
 
         /* void move (float speed) {
@@ -124,5 +169,23 @@ public class PlayerScripts : MonoBehaviour {
             otherPlayer.GetComponent<Rigidbody>().AddForce(dist * 20, ForceMode.Impulse);
         }
     }
-    
+   void Death()
+    {
+        if(SceneManager.GetActiveScene().name == "Singleplayer")
+        {
+            print("change scene");
+            SceneManager.LoadScene("GameOver");
+        }
+        else if(SceneManager.GetActiveScene().name == "Multiplayer")
+        {
+            print("Multiplayer Scene");
+        }
+    } 
+
+    public void BeingPushed()
+    {
+        timeSinceBeingPush = Time.time;
+        pushed = true;
+
+    }
 }
